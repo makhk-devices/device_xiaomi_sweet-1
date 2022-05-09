c62beb20622c547c7516cff1a0706e969abe0424
makhk <harikrish2050@gmail.com>
2022-05-09 13:15:34 +0530

    sweet: Back to Lineage
    This reverts commit 09abdfbf473c826e838770bbe84ef294754b805d.

--- device.mk
+++ device.mk
@@ -77,7 +77,7 @@ PRODUCT_COPY_FILES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-aosp
+    $(LOCAL_PATH)/overlay-lineage
 
 # Overlay-RRO
 PRODUCT_PACKAGES += \
--- lineage_sweet.mk
+++ lineage_sweet.mk
@@ -11,14 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common PixelOS stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-TARGET_GAPPS_ARCH := arm64
-TARGET_BOOT_ANIMATION_RES := 1080
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_sweet
-
+PRODUCT_NAME := lineage_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro

77bd02693e8f694141b4925ae23be3f5ec539e57
makhk <harikrish2050@gmail.com>
2022-05-10 12:19:14 +0530

    sweet: AEX - Go Official

--- aosp_sweet.mk
+++ aosp_sweet.mk
@@ -11,7 +11,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common AOSP stuff.
+# Inherit common AEX configurations
 $(call inherit-product, vendor/aosp/common.mk)
 
 TARGET_GAPPS_ARCH := arm64
@@ -26,3 +26,15 @@ PRODUCT_MODEL := Redmi Note 10 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# AEX - Go Official
+EXTENDED_BUILD_TYPE := OFFICIAL
+
+# Build with GApps if GAPPS_BUILD is true
+ifeq ($(GAPPS_BUILD),true)
+    WITH_GMS := true
+    WITH_GAPPS := true
+    TARGET_GAPPS_ARCH := arm64
+    IS_PHONE := true
+    TARGET_SHIPS_SEPERATE_GAPPS_BUILD := true
+endif
